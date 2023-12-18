3a57655ecec6a7ec24b63e2a61f664335e9efc1e
Naresh Reddy <bluebird.460@gmail.com>
2023-12-18 14:19:49 +0530

    xml updated

---
+++ src/main/java/com/restA/CopyGetresponseDemo.java
@@ -0,0 +1,59 @@
+package com.restA;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import io.restassured.specification.RequestSpecification;
+
+public class CopyGetresponseDemo {
+	
+	public static void main(String[] args) throws IOException {
+		getResponsemethod("/148703/latest",0, "UTI Nifty 200");
+		getResponsemethod("/143903/latest",1,"ICICI Bharat");
+		getResponsemethod("/120828/latest",2,"Quant Small Cap");
+		getResponsemethod("/150679/latest",3,"SBI ");
+		getResponsemethod("/120591/latest",4,"ICICI Small Cap");
+	}
+	
+	public static void getResponsemethod(String url,int sheetindex,String mfNmae) throws IOException {
+		RestAssured.baseURI = "https://api.mfapi.in/mf";
+		System.out.println("Mutual fund name is -->"+mfNmae);
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get(url);
+		JsonPath  jsonPathEvaluator= response.jsonPath();
+        String date = jsonPathEvaluator.getString("data[0].date");
+        String nav = jsonPathEvaluator.getString("data[0].nav");
+
+		System.out.println("fundhouse Body is: " + date);
+		System.out.println("fundhouse Body is: " + nav);
+		
+		FileInputStream fis= new FileInputStream(System.getProperty("user.dir") +"\\ExcelData\\Portfolio.xlsx");
+		
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet=workbook.getSheetAt(sheetindex);
+		int rowCount=sheet.getLastRowNum()+1;
+		System.out.println(rowCount);
+		//System.out.println(rowCount+1);
+		 Row rowHeading = sheet.createRow(rowCount);
+	        rowHeading.createCell(0).setCellValue(date);
+	        rowHeading.createCell(1).setCellValue(nav);
+	        
+	        FileOutputStream out = new FileOutputStream(System.getProperty("user.dir") +"\\ExcelData\\Portfolio.xlsx");
+	        workbook.write(out);
+	        out.flush();
+	        out.close();
+
+		        
+		        	 
+	}
+
+}
---
+++ src/main/java/com/restA/GoogleMFAPI.java
@@ -0,0 +1,99 @@
+package com.restA;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import io.restassured.specification.RequestSpecification;
+
+public class GoogleMFAPI {
+	private static Sheets sheetsService;
+	static String sheetName;
+
+	@Test
+	public static void mainTest() throws IOException {
+
+		String spreadsheetId = "1PiUPE5XaN83h6W14jjTN0QpJzA2J3SN8OQXnrb5V11E";
+		String sheetName = "Sheet1"; // Update with your sheet name
+
+		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("credentials.json"))
+				.createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+
+		// Initialize the Sheets service
+		sheetsService = new Sheets.Builder(new NetHttpTransport(), GsonFactory.getDefaultInstance(),
+				new HttpCredentialsAdapter(credentials)).setApplicationName("Portfolio").build();
+
+		getResponsemethod("/148703/latest", "UTI Nifty 200", spreadsheetId, "UTI");
+		getResponsemethod("/143903/latest", "ICICI Bharat", spreadsheetId, "ICICI");
+		getResponsemethod("/120828/latest", "Quant Small Cap", spreadsheetId, "QUANT");
+		getResponsemethod("/150679/latest", "SBI ", spreadsheetId, "SBI");
+		getResponsemethod("/120591/latest", "ICICI Small Cap", spreadsheetId, "ICICI SMALL");
+	}
+
+	public static void getResponsemethod(String url, String mfNmae, String spreadsheetId, String sheetName)
+			throws IOException {
+
+		RestAssured.baseURI = "https://api.mfapi.in/mf";
+		System.out.println("Mutual fund name is -->" + mfNmae);
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get(url);
+		JsonPath jsonPathEvaluator = response.jsonPath();
+		String date = jsonPathEvaluator.getString("data[0].date");
+		String nav = jsonPathEvaluator.getString("data[0].nav");
+
+		System.out.println("fundhouse Body is: " + date);
+		System.out.println("fundhouse Body is: " + nav);
+
+		int lastRow = getLastDataRow(spreadsheetId, sheetName);
+		int newRow = lastRow + 1;
+		String range = sheetName + "!A" + newRow + ":C" + newRow;
+
+		ValueRange body = new ValueRange().setValues(Arrays.asList(Arrays.asList(date, nav)));
+
+		try {
+
+			UpdateValuesResponse result = sheetsService.spreadsheets().values().update(spreadsheetId, range, body)
+					.setValueInputOption("RAW").execute();
+
+			System.out.println("Updated " + result.getUpdatedCells() + " cells");
+
+		} catch (GoogleJsonResponseException e) {
+			if (e.getStatusCode() == 400) {
+				System.err.println("Error details: " + e.getDetails());
+			} else {
+				throw e;
+			}
+		}
+	}
+
+	private static int getLastDataRow(String spreadsheetId, String sheetName) throws IOException {
+		String range = sheetName + "!A:A";
+		ValueRange response = sheetsService.spreadsheets().values().get(spreadsheetId, range).execute();
+		List<List<Object>> values = response.getValues();
+
+		return (values != null) ? values.size() : 0;
+	}
+
+}
